33d6c090bde03c4e5946a893395ac7988ffaa558
kolujAs <fernandodeibe@gmail.com>
2025-08-15 01:09:33 +0000

    feat: Muestra la ficha correctamente y comprueba al ganador

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const gameboard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
 
   function setMark(indice, ficha) {
-    board[indice] = ficha;
+    if (board[indice] === "") {
+      board[indice] = ficha;
+    } else {
+      console.log("PERDISTE EL TURNO POR BOBO");
+    }
   }
 
   function getBoard() {
@@ -28,6 +32,66 @@ const game = (function () {
 
   let jugadorActivo = player1;
 
+  const comprobarVictoria = () => {
+    const tableroActual = gameboard.getBoard();
+
+    if (
+      (tableroActual[0] === player1.mark &&
+        tableroActual[1] === player1.mark &&
+        tableroActual[2] === player1.mark) ||
+      (tableroActual[0] === player1.mark &&
+        tableroActual[3] === player1.mark &&
+        tableroActual[6] === player1.mark) ||
+      (tableroActual[0] === player1.mark &&
+        tableroActual[4] === player1.mark &&
+        tableroActual[8] === player1.mark) ||
+      (tableroActual[1] === player1.mark &&
+        tableroActual[4] === player1.mark &&
+        tableroActual[7] === player1.mark) ||
+      (tableroActual[2] === player1.mark &&
+        tableroActual[5] === player1.mark &&
+        tableroActual[8] === player1.mark) ||
+      (tableroActual[2] === player1.mark &&
+        tableroActual[4] === player1.mark &&
+        tableroActual[6] === player1.mark) ||
+      (tableroActual[3] === player1.mark &&
+        tableroActual[4] === player1.mark &&
+        tableroActual[5] === player1.mark) ||
+      (tableroActual[6] === player1.mark &&
+        tableroActual[7] === player1.mark &&
+        tableroActual[8] === player1.mark)
+    ) {
+      console.log(`Ganó el forro de ${player1.name}`);
+    } else if (
+      (tableroActual[0] === player2.mark &&
+        tableroActual[1] === player2.mark &&
+        tableroActual[2] === player2.mark) ||
+      (tableroActual[0] === player2.mark &&
+        tableroActual[3] === player2.mark &&
+        tableroActual[6] === player2.mark) ||
+      (tableroActual[0] === player2.mark &&
+        tableroActual[4] === player2.mark &&
+        tableroActual[8] === player2.mark) ||
+      (tableroActual[1] === player2.mark &&
+        tableroActual[4] === player2.mark &&
+        tableroActual[7] === player2.mark) ||
+      (tableroActual[2] === player2.mark &&
+        tableroActual[5] === player2.mark &&
+        tableroActual[8] === player2.mark) ||
+      (tableroActual[2] === player2.mark &&
+        tableroActual[4] === player2.mark &&
+        tableroActual[6] === player2.mark) ||
+      (tableroActual[3] === player2.mark &&
+        tableroActual[4] === player2.mark &&
+        tableroActual[5] === player2.mark) ||
+      (tableroActual[6] === player2.mark &&
+        tableroActual[7] === player2.mark &&
+        tableroActual[8] === player2.mark)
+    ) {
+      console.log(`Ganó el forro de ${player2.name}`);
+    }
+  };
+
   const jugarTurno = (casilla) => {
     if (jugadorActivo === player1) {
       gameboard.setMark(casilla, player1.mark);
@@ -38,7 +102,9 @@ const game = (function () {
     }
 
     console.log(gameboard.getBoard());
+
+    comprobarVictoria();
   };
 
-  return { jugarTurno };
+  return { jugarTurno, comprobarVictoria };
 })();
